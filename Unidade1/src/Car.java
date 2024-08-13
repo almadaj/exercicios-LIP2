e82ae88961d244077fba8e8d9a4e92d24cbe33a9
almadaj <julianoalmada48@gmail.com>
2024-08-12 21:48:19 -0300

    Introdução a POO

---
+++ Unidade1/src/Car.java
@@ -0,0 +1,23 @@
+public class Car {
+    int numberWheels;
+    int numberDoors;
+    String color;
+    Car(){
+        System.out.println("Car created!");
+        numberDoors = 4;
+        numberWheels = 4;
+        color = "red";
+    }
+    void accelerate(){
+        System.out.println("Vrummm");
+    }
+    void brake(){
+        System.out.println("Breeaaaking...");
+    }
+
+    void listInfo(){
+        System.out.println(color);
+        System.out.println(numberDoors);
+        System.out.println(numberWheels);
+    }
+}
