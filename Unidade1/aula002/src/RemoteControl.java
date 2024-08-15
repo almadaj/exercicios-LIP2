f8921a37a4ee95b1a7d4bb785927f11c8bd14b98
Juliano Almada <julianoalmada48@gmail.com>
2024-08-14 21:14:46 -0300

    Introdução a cardinalidade

---
+++ Unidade1/aula002/src/RemoteControl.java
@@ -0,0 +1,34 @@
+public class RemoteControl{
+  public int channel;
+  public int volume;
+
+  public RemoteControl(){
+    channel = 0;
+    volume = 0;
+  }
+
+  public void increaseChannel(){
+    this.channel++;
+  }
+
+  public void decreaseChannel(){
+    this.channel--;
+  }
+
+  public void increaseVolume(){
+    this.volume++;
+  }
+
+  public void decreaseVolume(){
+    this.volume--;
+  }
+
+  public void changeChannel(int newChannel){
+    this.channel = newChannel;
+  }
+
+  public void showStatus(){
+    System.out.println("Channel: " + channel);
+    System.out.println("Volume: " + volume);
+  }
+}
