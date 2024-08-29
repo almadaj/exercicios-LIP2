8377b0552c0a3054380183658c9ed93d6ff3f2a5
Juliano Almada <julianoalmada48@gmail.com>
2024-08-28 21:55:09 -0300

    Aula de encapsulamento

---
+++ Unidade1/src/Aula005/Account.java
@@ -0,0 +1,20 @@
+public class Account{
+  private double amount;
+
+  public Account(double amount) {
+    this.amount = amount;
+  }
+
+  public double getAmount() {
+    return amount;
+  }
+
+  public void addAmount(double deposit) {
+    this.amount += deposit;
+  }
+
+  public void withdraw(double withdraw) {
+    this.amount -= withdraw;
+  }
+
+}
